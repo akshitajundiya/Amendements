dcc59a8b571a5f778a54efd83dca99fac1966496
Akshita Jundiya <156312288+akshitajundiya@users.noreply.github.com>
2026-02-17 11:18:41 +0530

    Update abc.cpp

--- abc.cpp
+++ abc.cpp
@@ -1,4 +1,4 @@
-class Solution {asdfgnm
+class Solution {asdfgnmqwert
 public:
     int maxKDivisibleComp(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
         vector<vector<int> > G(n);
