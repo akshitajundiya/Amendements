674602cc06e243973ef5682bddae44469109583b
Akshita Jundiya <156312288+akshitajundiya@users.noreply.github.com>
2026-02-17 11:18:53 +0530

    Fix class definition and formatting in abc.cpp

--- abc.cpp
+++ abc.cpp
@@ -1,4 +1,4 @@
-class Solution {asdfgnmqwert
+class Solution {
 public:
     int maxKDivisibleComp(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
         vector<vector<int> > G(n);
